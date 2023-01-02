f510845164bad396aa5baa902a0fd5ca938c0ddf
ashtondunderdale <ashtondunderdale@gmail.com>
2023-01-02 00:43:22 +0000

    created slot machine
    created slot machine code

--- code_2023/randomSlotMachine.py
+++ code_2023/randomSlotMachine.py
@@ -1,14 +1,34 @@
 import random
 
-slotSymbols = ["cherry", "lemon", "orange", "plum", "bell", "bar"]
+symbols = ["seven", "diamond", "cherry"]
+prizes = [100, 50, 10]
 
-def spinSlots():
-  symbol1 = random.choice(slotSymbols)
-  symbol2 = random.choice(slotSymbols)
-  symbol3 = random.choice(slotSymbols)
+
+def spin():
+  """generates 3 slot machine symbols, returns as list"""
+
+  symbol1 = random.choice(symbols)
+  symbol2 = random.choice(symbols)
+  symbol3 = random.choice(symbols)
 
   return [symbol1, symbol2, symbol3]
 
-result = spinSlots()
 
+def get_prize(symbols, prizes):
+  """gets the index of the winning symbol and allocated prize for the symbol index"""
+
+  index = symbols.index(result[0])
+
+  return prizes[index]
+
+
+result = spin()
 print(result)
+
+# checks if symbols are same, gets prize for that match, outputs winnings
+if result[0] == result[1] == result[2]:
+  prize = get_prize(symbols, prizes)
+  print(f"You won ${prize}! You got three {result[0]}s in a row")
+
+else:
+  print("No Match")
